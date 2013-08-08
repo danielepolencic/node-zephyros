a9e8a53dde2e8cae752d453a4bd854b2a63ca697
Daniele Polencic <daniele.polencic@gmail.com>
2013-08-08 21:32:37 +0100

    playing with the demo.

--- demo/demo.js
+++ demo/demo.js
@@ -15,13 +15,61 @@ z.bind('r', ['Cmd', 'Shift']).thenFocusedWindow().then(function(window){
   console.log("window: ", window);
 });
 
-z.bind('e', ['Cmd', 'Shift'])
+z.bind('right', ['Cmd', 'Alt', 'Ctrl'])
 .thenFocusedWindow()
 .thenGetWindowFrame()
-.then(function(frame){
-  console.log("frame: ", frame);
-  return frame;
-})
 .thenSetWindowFrame(function(window){
-  return { id: window.id, frame : { x: 0, y: 22, w: 300, h: 300 } };
+  var grid = toGridCoordinates( window.frame );
+  grid.w += 1;
+  return { id: window.id , frame: toFramePixels( grid ) };
 });
+
+z.bind('left', ['Cmd', 'Alt', 'Ctrl'])
+.thenFocusedWindow()
+.thenGetWindowFrame()
+.thenSetWindowFrame(function(window){
+  var grid = toGridCoordinates( window.frame );
+  grid.w -= 1;
+  return { id: window.id , frame: toFramePixels( grid ) };
+});
+
+z.bind('down', ['Cmd', 'Alt', 'Ctrl'])
+.thenFocusedWindow()
+.thenGetWindowFrame()
+.thenSetWindowFrame(function(window){
+  var grid = toGridCoordinates( window.frame );
+  grid.h += 1;
+  return { id: window.id , frame: toFramePixels( grid ) };
+});
+
+z.bind('up', ['Cmd', 'Alt', 'Ctrl'])
+.thenFocusedWindow()
+.thenGetWindowFrame()
+.thenSetWindowFrame(function(window){
+  var grid = toGridCoordinates( window.frame );
+  grid.h -= 1;
+  return { id: window.id , frame: toFramePixels( grid ) };
+});
+
+
+function toGridCoordinates( frame ){
+  var column_size = 240;
+  var row_size = 225;
+  return {
+    x: Math.round(frame.x / column_size),
+    y: Math.round(frame.y / row_size),
+    w: Math.round(frame.w / column_size),
+    h: Math.round(frame.h / row_size)
+  };
+}
+
+function toFramePixels( grid ) {
+  var column_size = 240;
+  var row_size = 225;
+  return {
+    x: grid.x * column_size,
+    y: grid.y * row_size,
+    w: grid.w * column_size,
+    h: grid.h * row_size
+  };
+}
